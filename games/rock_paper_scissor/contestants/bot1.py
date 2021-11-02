3de251391061021d4344f641fa6e33b9626ea803
Viggo T. Wivestad <viggotw@gmail.com>
2021-11-02 13:31:29 +0100

    Rename players to bots

--- games/rock_paper_scissor/contestants/bot1.py
+++ games/rock_paper_scissor/contestants/bot1.py
@@ -1,5 +1,17 @@
-from games.game_log import GameLog
 from games.rock_paper_scissor.contestant_custom import ContestantCustom, Action
+
+''' GAME_LOG STRUCTURE
+{
+    'me': {
+        'score': 0,
+        'moves': []
+    },
+    'opponents': {
+        'score': [0],
+        'moves': [[]]
+    }
+}
+'''
 
 class Contestant(ContestantCustom):
     def action(self, game_log:dict) -> Action.__mro__[0]:
--- games/rock_paper_scissor/contestants/bot2.py
+++ games/rock_paper_scissor/contestants/bot2.py
@@ -2,6 +2,19 @@ from games.game_log import GameLog
 from games.rock_paper_scissor.contestant_custom import ContestantCustom, Action
 from random import choice
 
+''' GAME_LOG STRUCTURE
+{
+    'me': {
+        'score': 0,
+        'moves': []
+    },
+    'opponents': {
+        'score': [0],
+        'moves': [[]]
+    }
+}
+'''
+
 class Contestant(ContestantCustom):
     def action(self, game_log:dict) -> Action.__mro__[0]:
         return choice([Action.ROCK, Action.PAPER, Action.SCISSOR])
